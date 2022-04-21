7988520fb33c6e5d3a3617288784e3f21969dfe8
Leandro Souza <103716539+leandro-souza08@users.noreply.github.com>
2022-04-21 17:16:14 -0300

    aprimorando funcao pre-load

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+document.body.onload = function(){
+
+  var preLoad = document.querySelector(".sessao-pre-load");
+  var container = document.querySelector(".container");
+    
+  setTimeout(function(){
+    
+  container.style.display = "block";
+  preLoad.style.display = "none";
+  
+  }, 5000);
+    
+  };
+
+
 var hamburger = document.querySelector(".hamburguer").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("show-menu");
 });
+
