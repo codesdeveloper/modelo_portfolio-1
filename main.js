105d1abea0f841315353e7bd6da03e5bbf585442
leandroc2w@gmai.com <leandroc2w@gmai.com>
2022-04-22 13:42:08 -0300

    quase pronto

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
-var start = function(){
-
-  var preLoad = document.querySelector(".sessao-pre-load");
-  var container = document.querySelector(".container");
-    
-  setTimeout(function(){
-    
-  container.style.display = "block";
-  preLoad.style.display = "none";
-  
-  }, 5000);
-    
-  };
-
+document.body.onload = function() {
+  setTimeout(function() {
+    var preLoad = document.querySelector(".sessao-pre-load");
+    var container = document.querySelector(".container");
+    container.style.display = "block";
+    preLoad.style.display = "none";
+  }, 2000);
+}
 
 var hamburger = document.querySelector(".hamburguer").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("show-menu");
 });
-
