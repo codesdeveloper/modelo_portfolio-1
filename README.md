9e7cb21dffb23b8782979bb7d7445f37202e6b69
leandro <leandros8w@gmail.com>
2022-09-06 05:13:31 -0300

    progresso,  ta evoluindo hehe

--- README.md
+++ README.md
@@ -11,3 +11,16 @@ O controle das tarefas desse projeto serão realizadas no github
 :up: atualização
 :beatle: correção de bug
 :checkered_flag: release
+
+
+#Muita coisa foi adiantado 
+
+#Resta fazer:
+
+
+[ ] Responsivel da area de contato
+[ ] Responsivel da area de orçamento
+[ ] Responsivel da rodape
+[ ] Animação de iniciação
+[ ] Video demostrativo
+[ ] Posta e divulgar
--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-document.body.onload = function() {
-  setTimeout(function() {
-    var preLoad = document.querySelector(".sessao-pre-load");
-    var container = document.querySelector(".container");
-    container.style.display = "block";
-    preLoad.style.display = "none";
-  }, 0);
-}
-
 var hamburger = document.querySelector(".hamburguer").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("show-menu");
 });
